e1cde341c30c7675acba2064e5ff8640694952e6
Robert <robert.hendrickson15@gmail.com>
2024-02-02 08:59:26 -0700

    fixed some bugs. Added a video walk through. Added a home page button and update for the new tool

--- js/fee-difference-calc.js
+++ js/fee-difference-calc.js
@@ -23,6 +23,7 @@ var tableObject = {
     },
 };
 
+//controls if amex rate boxes are visible or hidden.
 function amexDisplayToggle(){
     let checked = $('input#amexCheck')[0].checked;
     if(checked){
@@ -33,6 +34,7 @@ function amexDisplayToggle(){
     };
 };
 
+//hides or displays import popup
 function importToggle(action){
     if(action === 'open'){
         $('.popup1').removeClass('hide');
@@ -41,6 +43,7 @@ function importToggle(action){
         $('.popup1').addClass('hide');
     };
 };
+
 //closes the wait box and removes any existing error message text
 function closeDialogue(){
     $('.popup2').addClass('hide');
@@ -71,6 +74,7 @@ function messageUpdate(message){
     $('.errorMessage')[0].append($.parseHTML(`<p>${message}</p>`)[0]);
 };
 
+//takes import data and uses object method to add data to a loopable object
 function buildObject(){
     if(checkNumberOfLines()){
         let split_data = _import.match(/(\d{2}:\d{2}:\d{2} [APMapm]{2})\n(\d+)\n(.*?)\n(.*?)\n([A-Za-z]{3} \d{1,2}, \d{4})\n(-{0,1}\$[\d.,]+)\n(-{0,1}\$[\d.,]+)\n(-{0,1}\$[\d.,]+)\n(-{0,1}\$[\d.,]+)/g);
@@ -79,7 +83,9 @@ function buildObject(){
     };
 };
 
+//uses tableObject to create table data
 function buildTable(){
+    $('#transactions tbody')[0].innerHTML = '';
     for(const row in tableObject.data){
         let tr = $.parseHTML(`<tr id="${tableObject.data[row].row}"><td>${tableObject.data[row].dateNum}</td><td>${tableObject.data[row].transaction}</td><td>${tableObject.data[row].atTill}</td><td>${tableObject.data[row].status}</td><td>${tableObject.data[row].dateText}</td><td>${tableObject.data[row].collected}</td><td>${tableObject.data[row].fee}</td><td>${tableObject.data[row].return}</td><td>${tableObject.data[row].totalPayout}</td><td></td><td></td>`)[0];
         $('#transactions tbody')[0].append(tr);
@@ -95,6 +101,7 @@ function precentToDecimal(value){
     return value;
 };
 
+//loops through table and adds info to last two cells of each row
 function calculateDifference(){
     let rates = {};
     //collect and store card rates and fees
@@ -106,7 +113,7 @@ function calculateDifference(){
     let table_data = $('#transactions tbody tr');
     for(i=0;i<table_data.length;i++){
         temp_row = table_data[i];
-        if(temp_row.querySelector('td:nth-child(3)').innerText === 'Card Not Present'){
+        if(temp_row.querySelector('td:nth-child(3)').innerText === 'Card Not Present' && temp_row.querySelector('td:nth-child(6)').innerText !='$0.00'){
             temp_rates = rates.cardp;
             let temp_collected = temp_row.querySelector('td:nth-child(6)').innerText.replace('$','');
             if($('#amexCheck')[0].checked){
@@ -125,19 +132,25 @@ function calculateDifference(){
             difference += temp_fee;
             temp_row.querySelector('td:nth-child(10)').innerText = '$' + temp_fee.toFixed(2);
             temp_row.querySelector('td:last-child').innerText = '$' + difference.toFixed(2);
+        }else{
+            temp_row.querySelector('td:nth-child(10)').innerText = '$0.00';
+            temp_row.querySelector('td:last-child').innerText = '$0.00';
         };
     };
     calculateTotals(table_data);
 };
 
+//loops through table to compile totals
 function calculateTotals(table){
     let total_fees = 0;
     let total_calculated_fees = 0;
     let total_difference = 0;
     for(i=0;i<table.length;i++){
-        total_fees += parseFloat(table[i].querySelector('td:nth-child(7)').innerText.replace('$',''));
-        total_calculated_fees += parseFloat(table[i].querySelector('td:nth-child(10)').innerText.replace('$',''));
-        total_difference += parseFloat(table[i].querySelector('td:nth-child(11)').innerText.replace('$',''));
+        if(table[i].querySelector('td:nth-child(10)').innerText != '$0.00'){
+            total_fees += parseFloat(table[i].querySelector('td:nth-child(7)').innerText.replace('$',''));
+            total_calculated_fees += parseFloat(table[i].querySelector('td:nth-child(10)').innerText.replace('$',''));
+            total_difference += parseFloat(table[i].querySelector('td:nth-child(11)').innerText.replace('$',''));
+        };
     };
     let total_table = $('#totals tbody tr')[0];
     total_table.querySelector('td:nth-child(1)').innerText = `$${total_fees.toFixed(2)}`;
@@ -145,28 +158,35 @@ function calculateTotals(table){
     total_table.querySelector('td:nth-child(3)').innerText = `$${total_difference.toFixed(2)}`;
 };
 
-/*This is a usable regex that will find all occurances from a coppied set of data out of rain.
-(\d{2}:\d{2}:\d{2} [APMapm]{2})\n(\d+)\n(.*?)\n(.*?)\n([A-Za-z]{3} \d{1,2}, \d{4})\n(-{0,1}\$[\d.,]+)\n(-{0,1}\$[\d.,]+)\n(-{0,1}\$[\d.,]+)\n(-{0,1}\$[\d.,]+)
+//removes existing data in the table and resets object
+function resetTable(){
+    $('#transactions tbody')[0].innerHTML = '';
+    calculateTotals($('#transactions tbody')[0]);
+    tableObject.data = {};
+    tableObject.rows = 0;
+};
 
-can be used with x.match(/regex/g) to find all transactions
-y[i].split('\n') can find each piece of the lines to create object based variables.
-
-{
-    dateNum: [0],
-    transaction: [1],
-    atTill: [2],
-    status: [3],
-    dateText: [4],
-    collected: [5],
-    fee: [6],
-    return: [7],
-    totalPayout: [8]
+//toggles video popup
+function videoToggle(action){
+    if(action === 'open'){
+        $('.video').removeClass('hide');
+    };
+    if(action === 'close'){
+        $('.video').addClass('hide');
+    };
 }
+
+/*
+card rates
+1.95,0.07
+2.9,0.3
+2.1,0.15
+3.2,0.35
 
 test string: should have 5 transactions listed
 04:35:05 AM
 1400120209
-Card Not Present
+Card Present
 Completed
 Jan 17, 2024
 $24.28
@@ -176,10 +196,10 @@ $23.28
 04:35:05 AM
 1400120210
 Card Not Present
-Completed
+Amex
 Jan 17, 2024
 $24.28
--$1.00
+-$1.13
 $0.00
 $23.28
 04:35:06 AM
