32b85bffa0a801ce2126f735398f1a3783dbd51e
Robert <robert.hendrickson15@gmail.com>
2024-02-27 08:52:55 -0700

    changed function to accept an object and build the vue template based on it's data

--- common/actions/popup.js
+++ common/actions/popup.js
@@ -1,15 +1,22 @@
 let growl;
-function test(value){
-    if($('#test')[0].__vue_app__){
+function test(error_object){
+    //set an empty html string to be edited later
+    let template_html = '';
+    //loop through object to make the template based on object data
+    for (const error in error_object) {
+        template_html += `<div>{{${error}}}</div><br />`;
+    };
+    //if the target element has a mount running, unmount it (likely to be moved to a different function later for closing popup)
+    if($('#error_message')[0].__vue_app__){
         growl.unmount();
     };
-    growl= Vue.createApp({
-        template:`<div>{{test}}</div>`,
+    //create vue app with template and data return points
+    growl = Vue.createApp({
+        template: template_html,
         data() {
-            return {
-                test: value
-            }
+            return error_object
         }
     });
-    growl.mount('#test');
+    //mount the vue app
+    growl.mount('#error_message');
 };
