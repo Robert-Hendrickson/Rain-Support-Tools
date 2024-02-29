873e6f2256605861ccc21ec83afb71fe00795fa8
Robert <robert.hendrickson15@gmail.com>
2024-02-28 17:02:09 -0700

    added css for popup box

--- common/actions/popup.js
+++ common/actions/popup.js
@@ -1,11 +1,13 @@
 let error_growl;
 function popup_error_growl(error_object){
     //set an empty html string to be edited later
-    let template_html = '';
+    let template_html = 'These are the issues found with the data provided:<span onclick="Close_error_growl()">X</span><ul>';
     //loop through object to make the template based on object data
     for (const error in error_object) {
-        template_html += `<div>{{${error}}}</div><br />`;
+        template_html += `<li>{{${error}}}</li><br />`;
     };
+    //close ul element
+    template_html += `</ul>`;
     //if the target element has a mount running, unmount it (likely to be moved to a different function later for closing popup)
     if($('#error_message')[0].__vue_app__){
         error_growl.unmount();
@@ -19,7 +21,9 @@ function popup_error_growl(error_object){
     });
     //mount the vue app
     error_growl.mount('#error_message');
+    $('#error_message').removeClass('hide');
 };
 function Close_error_growl(){
-
+    error_growl.unmount();
+    $('#error_message').addClass('hide');
 };
--- js/ticket_form.js
+++ js/ticket_form.js
@@ -192,7 +192,7 @@ function checkInputs() {
         };
         if(!inputs.example | regex.na.test(inputs.example) | !exampleCheck()){
             addBorder('#example_input');
-            error_list['example'] = `Please make sure that the data in examples meet the expected criteria. 1. Cannot be blank 2. Cannot be N/A 3. Given links cannot point back to admin domains (rainadmin.com/quiltstorewebsites.com)`;
+            error_list['example'] = `Please make sure that the data in examples meet the expected criteria. 1.Cannot be blank. 2.Cannot be N/A 3.Given links cannot point back to admin domains (rainadmin.com/quiltstorewebsites.com)`;
         };
         if(exampleCheck()){
             $('#error-wrapper').removeClass("example");
@@ -222,7 +222,7 @@ function checkInputs() {
         };
         if(!inputs.console){
             addBorder('#console_input');
-            error_list['console'] = 'The Console Errors is blank.';
+            error_list['consoleerrors'] = 'The Console Errors is blank.';
         };
         $('#error-borders')[0].innerHTML = `
             ${borders} {
