152ac92e4014048fde206399a17764554290db4f
Robert <robert.hendrickson15@gmail.com>
2024-03-14 08:42:02 -0600

    added update info to index page and a link to the new page

--- js/nav-builder.js
+++ js/nav-builder.js
@@ -1,6 +1,7 @@
 let navigation = document.querySelector('.toolButtonsList');
 const nav_list ={
 'Bug Ticket Form': 'Bug_ticket_form.html',
+'Site Fix Ticket': 'site-fix.html',
 'Transaction Calculator': 'Javascript_calculator.html',
 'Transaction Calculator<br />(Tax Inclusive: Australia)': 'Tax-Inclusive-Calculator.html',
 'Washington Tax Rate': 'wa_tax_rates.html',
