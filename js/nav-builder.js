bc3b338b8f06f5b7bce14b71a128db8e2ca1dcbc
Robert <robert.hendrickson15@gmail.com>
2024-03-26 09:09:28 -0600

    changed main nav builder to use vue

--- js/nav-builder.js
+++ js/nav-builder.js
@@ -1,19 +1,16 @@
-let navigation = document.querySelector('.toolButtonsList');
-const nav_list ={
-'Bug Ticket Form': 'Bug_ticket_form.html',
-'Site Fix Ticket': 'site-fix.html',
-'Transaction Calculator': 'Javascript_calculator.html',
-'Transaction Calculator<br />(Tax Inclusive: Australia)': 'Tax-Inclusive-Calculator.html',
-'Washington Tax Rate': 'wa_tax_rates.html',
-'Fee Difference Calculator': 'fee-difference-calculator.html',
-'QB Payment Split': 'QB_Mixed_Split.html',
-'Inventory Check': 'Inventory-check.html',
-'CSS Sandbox': 'css-tester.html'
-};
-let nav_element = '';
-for(const page in nav_list){
-nav_element += `<div class="toolButton">
-<button onclick="location.pathname = '/Rain-Support-Tools/${nav_list[page]}'">${page}</button>
-</div>`;
-};
-document.querySelector('.toolButtonsList').innerHTML = nav_element;
+const app = Vue.createApp({
+    data() {
+        return {nav_list : [
+            {name:'Bug Ticket Form', url: 'Bug_ticket_form.html'},
+            {name:'Site Fix Ticket', url: 'site-fix.html'},
+            {name:'Transaction Calculator', url: 'Javascript_calculator.html'},
+            {name:'Transaction Calculator (Tax Inclusive: Australia)', url: 'Tax-Inclusive-Calculator.html'},
+            {name:'Washington Tax Rate', url: 'wa_tax_rates.html'},
+            {name:'Fee Difference Calculator', url: 'fee-difference-calculator.html'},
+            {name:'QB Payment Split', url: 'QB_Mixed_Split.html'},
+            {name:'Inventory Check', url: 'Inventory-check.html'},
+            {name:'CSS Sandbox', url: 'css-tester.html'}
+        ]};
+    }
+});
+app.mount('.toolButtonsList');
