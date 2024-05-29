cd5213e4482e3851aa551f3034c6fbe95aa0ee08
Robert <robert.hendrickson15@gmail.com>
2024-05-29 11:50:39 -0600

    changed how the nav menu is displayed so there are groups of links

--- js/nav-builder.js
+++ js/nav-builder.js
@@ -1,20 +1,28 @@
 const app = Vue.createApp({
     data() {
-        return {nav_list : [
-            {name: 'Home', url: '/Rain-Support-Tools/'},
-            //{name:'Bug Ticket Form', url: 'Bug_ticket_form.html'},
-            {name: 'Bug Ticket v2', url: 'bug-ticket-v2.html'},
-            {name:'Site Fix Ticket', url: 'site-fix.html'},
-            {name:'Transaction Calculator', url: 'Javascript_calculator.html'},
-            {name:'Transaction Calculator (Tax Inclusive: Australia)', url: 'Tax-Inclusive-Calculator.html'},
-            {name:'Washington Tax Rate', url: 'wa_tax_rates.html'},
-            {name:'Fee Difference Calculator', url: 'fee-difference-calculator.html'},
-            {name:'QB Payment Split', url: 'QB_Mixed_Split.html'},
-            {name:'Inventory Check', url: 'Inventory-check.html'},
-            {name:'CSS Sandbox', url: 'css-tester.html'},
-            {name:'Email List Checker', url: 'email-list-checker.html'},
-            {name:"Riley's Secret", url: 'riley/riley.html', target:'blank'}
-        ]};
+        return {nav_list : {
+                home: [
+                    {name: 'Home', url: '/Rain-Support-Tools/'}
+                ],
+                support: [
+                    //{name:'Bug Ticket Form', url: 'Bug_ticket_form.html'},
+                    {name: 'Bug Ticket v2', url: 'bug-ticket-v2.html'},
+                    {name:'Site Fix Ticket', url: 'site-fix.html'}
+                ],
+                system: [
+                    {name:'Email List Checker', url: 'email-list-checker.html'},
+                    {name:'Transaction Calculator', url: 'Javascript_calculator.html'},
+                    {name:'Transaction Calculator (Tax Inclusive: Australia)', url: 'Tax-Inclusive-Calculator.html'},
+                    {name:'Washington Tax Rate', url: 'wa_tax_rates.html'},
+                    {name:'Fee Difference Calculator', url: 'fee-difference-calculator.html'},
+                    {name:'QB Payment Split', url: 'QB_Mixed_Split.html'},
+                    {name:'Inventory Check', url: 'Inventory-check.html'}
+                ],
+                misc: [
+                    {name:'CSS Sandbox', url: 'css-tester.html'},
+                    {name:"Riley's Secret", url: 'riley/riley.html', target:'blank'}
+                ]
+            }};
     }
 });
 app.mount('.toolButtonsList');
