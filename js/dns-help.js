5da679c8f2a74c844a8d7c92c7cf13d83e9db5ff
Robert <robert.hendrickson15@gmail.com>
2024-06-07 14:53:18 -0600

    finished checks for first step and updated moving to second step to hide/display necessary panels

--- js/dns-help.js
+++ js/dns-help.js
@@ -1,8 +1,33 @@
+let action_type = [];
+function domaincheck() {
+    let domain = $('#domain')[0].value;
+    return RegExp(/^[\w\-]+\.\w{2,}$/).test(domain);
+}
 function validateData(){
+    Close_error_growl();
+    let bad_object = {
+        type: 'generate',
+        list: {}
+    };
     let current_step = parseInt($('[step].active').attr('step'));
     switch(current_step){
         case 1:
-            nextStep(current_step);
+            //check the domain has been entered and is a valid domain
+            if (!domaincheck()) {
+                bad_object.list['domain_check'] = 'Please make sure that the domain is correct and not blank.'
+            }
+            //check an action has been selected
+            if ($('[action].selected').length) {
+                action_type = Array.from($('[action].selected'), (el) => el.attributes.action.value);
+                console.log('actions: ' + action_type);
+            } else {
+                bad_object.list['action_check'] = 'Please select which record action(s) needs to be done.';
+            }
+            if (Object.entries(bad_object.list).length) {
+                popup_error_growl(bad_object);
+            } else {
+                nextStep(current_step);
+            };
             break;
         case 2:
             nextStep(current_step);
@@ -15,6 +40,8 @@ function validateData(){
 function nextStep(step_number){
     $(`[step="${step_number}"]`)[0].classList = 'complete';
     $(`[step="${step_number + 1}"]`)[0].classList = 'active';
+    $(`[data='${step_number}`).removeClass('active');
+    $(`[data='${step_number + 1}`).addClass('active');
     switch(step_number){
         case 1:
             $('[flow-controls] button[prev]').removeClass('hide');
@@ -29,19 +56,26 @@ function previousStep(){
     let step_number = parseInt($('[step].active').attr('step'));
     $(`[step="${step_number}"]`)[0].classList = '';
     $(`[step="${step_number - 1}"]`)[0].classList = 'active';
+    $(`[data='${step_number}`).removeClass('active');
+    $(`[data='${step_number - 1}`).addClass('active');
     switch(step_number){
         case 3:
             $('[flow-controls] button[finish]').addClass('hide');
             $('[flow-controls] button[next]').removeClass('hide');
             break;
         case 2:
-            $('[flow-controls] button[prev]').removeClass('hide');
+            $('[flow-controls] button[prev]').addClass('hide');
             break;
     }
 }
 function selectAction(el){
-    $('div[action].selected').removeClass('selected');
-    el.target.classList.value = 'selected';
+    if (el.target.classList.value === 'selected') {
+        $(el.target).removeClass('selected');
+        $(`#${el.target.attributes.action.value}`).hide();
+    } else {
+        $(el.target).addClass('selected');
+        $(`#${el.target.attributes.action.value}`).show();
+    }
 }
 
 $(window).ready(function (){
