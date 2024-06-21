8e98d8e3bf363086e6718d43e93c86005029674a
Robert <robert.hendrickson15@gmail.com>
2024-06-21 17:04:37 -0600

    updated nav builder to all be in the same script so it keeps build with it. Better for future updates. updated all pages to use new layout

--- js/nav-builder.js
+++ js/nav-builder.js
@@ -1,4 +1,76 @@
+let html;
+if((/Rain-Support-Tools\/$/).test(location.pathname)) {
+    html = `<div class="toolButtonsList">
+    <div id="support">
+        <div v-for="nav_item in nav_list.support" class="toolButton">
+            <a v-bind:href="nav_item.url" v-bind:target="nav_item.target">
+                <button>{{nav_item.name}} {{nav_item.subTitle}}</button>
+            </a>
+        </div>
+    </div>
+    <div id="system" style="display: none;">
+        <div v-for="nav_item in nav_list.system" class="toolButton">
+            <a v-bind:href="nav_item.url" v-bind:target="nav_item.target">
+                <button>{{nav_item.name}} {{nav_item.subTitle}}</button>
+            </a>
+        </div>
+    </div>
+    <div id="misc" style="display: none;">
+        <div v-for="nav_item in nav_list.misc" class="toolButton">
+            <a v-bind:href="nav_item.url" v-bind:target="nav_item.target">
+                <button>{{nav_item.name}} {{nav_item.subTitle}}</button>
+            </a>
+        </div>
+    </div>
+</div>`;
+} else {
+    html = `<div class="secondary_menu_links">
+    <div class="toolButtonsList">
+        <ul>
+            <li v-for="nav_item in nav_list.home" class="toolButton">
+                <a v-bind:href="nav_item.url" v-bind:target="nav_item.target">
+                    <div>
+                        <span v-bind:class="nav_item.icon"></span>{{nav_item.name}}<span class="sub-title">{{nav_item.subTitle}}</span>
+                    </div>
+                </a>
+            </li>
+        </ul>
+        <div class="nav-header">Support Tools</div>
+        <ul>
+            <li v-for="nav_item in nav_list.support" class="toolButton">
+                <a v-bind:href="nav_item.url" v-bind:target="nav_item.target">
+                    <div>
+                        <span v-bind:class="nav_item.icon"></span>{{nav_item.name}}<span class="sub-title">{{nav_item.subTitle}}</span>
+                    </div>
+                </a>
+            </li>
+        </ul>
+        <div class="nav-header">System Tools</div>
+        <ul>
+            <li v-for="nav_item in nav_list.system" class="toolButton">
+                <a v-bind:href="nav_item.url" v-bind:target="nav_item.target">
+                    <div>
+                        <span v-bind:class="nav_item.icon"></span>{{nav_item.name}}<span class="sub-title">{{nav_item.subTitle}}</span>
+                    </div>
+                </a>
+            </li>
+        </ul>
+        <div class="nav-header">Misc Tools</div>
+        <ul>
+            <li v-for="nav_item in nav_list.misc" class="toolButton">
+                <a v-bind:href="nav_item.url" v-bind:target="nav_item.target">
+                    <div>
+                        <span v-bind:class="nav_item.icon"></span>{{nav_item.name}}<span class="sub-title">{{nav_item.subTitle}}</span>
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>`
+}
+
 const app = Vue.createApp({
+    template: html,
     data() {
         return {nav_list : {
                 home: [
@@ -26,7 +98,7 @@ const app = Vue.createApp({
             }};
     }
 });
-app.mount('.toolButtonsList');
+app.mount('nav-menu');
 
 function toggleNav(){
     if($('.secondary_menu_links')[0].classList.value.match(/active/) != null){
