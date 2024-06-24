8b24076c035c5a0d31f4d7f79a02591daaf4151b
Robert <robert.hendrickson15@gmail.com>
2024-06-24 16:45:32 -0600

    fixed cname value check

--- js/dns-help.js
+++ js/dns-help.js
@@ -58,8 +58,8 @@ function checkValues(record_data){
             if (record_data.name === '') {
                 potential_errors['CNAME_record_name'] = 'Enter a value for the record Name';
             };*/
-            if (!(/^(?:\w+\.)?\w+\.\w{2,}$/).test(record_data.value)) {
-                potential_errors['CNAME_record_value'] = 'CNAME record value needs to be a domain. (domain.com)';
+            if (!(/^(?:[\w\-]+\.)+[\w\-]+\.\w{2,}\.?$/).test(record_data.value)) {
+                potential_errors['CNAME_record_value'] = 'CNAME record value needs to be a domain similar to the following pattern. (_data-string.(sub.)domain.com)';
             };
             break;
         case 'MX':
