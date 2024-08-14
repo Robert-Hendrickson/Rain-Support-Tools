97801dd287563998ec74c1ec78c1a1f582d64e6a
Robert <robert.hendrickson15@gmail.com>
2024-08-14 10:19:48 -0600

    updated call to banner function

--- common/ctrl/banner_updates.js
+++ common/ctrl/banner_updates.js
@@ -1,22 +1,16 @@
 function buildBanner(bannerArray = ['Test Title','Test content','banner-test']) {
-    $('body').prepend(`<div id="${bannerArray[2]}" class="banner-wrapper">
-        <div class="banner-content">
-            <h2>${bannerArray[0]}</h2>
-            <p>${bannerArray[1]}</p>
-        </div>
-        <button class="btn primary" onclick="closeBanner('${bannerArray[2]}')">Close</button>
-        </div>`);
+    if (!getCookie(bannerArray[2])) {
+        $('body').prepend(`<div id="${bannerArray[2]}" class="banner-wrapper">
+            <div class="banner-content">
+                <h2>${bannerArray[0]}</h2>
+                <p>${bannerArray[1]}</p>
+            </div>
+            <button class="btn primary" onclick="closeBanner('${bannerArray[2]}')">Close</button>
+            </div>`);
+    }
 }
 
 function closeBanner(bannerID){
     $(`#${bannerID}`).remove();
     setCookie(bannerID,'closed',14);
 }
-
-$(window).ready(function (){
-    if((/bug-ticket-v2/).test(location.pathname)){
-        if(!getCookie('banner-format')){
-            buildBanner(['Tool Update!','The expected Results field has been removed. You will no longer be required to enter the expected outcome of the system.', 'banner-format']);
-        }
-    }
-})
