767b87fbb9e0957c5684aac7c00f2fda9a815313
Robert <robert.hendrickson15@gmail.com>
2024-08-22 08:33:36 -0600

    updated search function, also added banner message for tool info

--- js/bandwidth-errors.js
+++ js/bandwidth-errors.js
@@ -1,8 +1,9 @@
 function displayMatchingRows(value){
+    let regex_string = value.replaceAll(' ','|');
     $('.error-list').each(function(){
         let found_row = false;
         $(this).children('.row-wrapper').each(function(){
-            if($(this).text().match(value)){
+            if($(this).text().match(regex_string)){
                 $(this).show();
                 found_row = true;
             } else {
