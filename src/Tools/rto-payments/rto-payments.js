0bb50b08d86fe951e96af0db50607e92e5a59854
Robert <robert.hendrickson15@gmail.com>
2025-01-27 16:14:16 -0700

    added form for rto data inputs and buttons. also started js file for functionality.

---
+++ src/Tools/rto-payments/rto-payments.js
@@ -0,0 +1,12 @@
+
+
+
+
+function calculateRTOPayments() {
+    // this funtion calculates the RTO Payments for the current RTO Payments form.
+    return 'Hello from calculateRTOPayments';
+}
+function clearRTOPayments() {
+    // This function removes all existing data from the RTO Payments table and resets values in the RTO Payments form.
+    return 'Hello from clearRTOPayments';
+}
