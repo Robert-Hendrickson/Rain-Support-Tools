5253f1fac5fd2bf045b402dbf230e737023278c3
Robert <robert.hendrickson15@gmail.com>
2025-04-16 11:31:09 -0600

    updated some changes to allow testing more easily

--- src/Tools/share-point/auth-config.js
+++ src/Tools/share-point/auth-config.js
@@ -2,5 +2,6 @@ export const config = {
     tenantId: atob('OTFmYmNkMDItMmE1ZC00ZWZhLTlhNDktYjE4ZTE3MzJlNDU2'), // Base64 encoded
     clientId: atob('ZTEwNGRmMDgtMDFkMi00Njc0LWExZGEtMDgwZjQ0Njc4NDc0'),
     redirectUri: 'https://robert-hendrickson.github.io/Rain-Support-Tools/src/Tools/share-point/callback.html',
+    testRedirectUri: 'http://localhost/Rain-Support-Tools/src/Tools/share-point/callback.html',
     scopes: 'user.read files.readwrite sites.readwrite.all'
 };
