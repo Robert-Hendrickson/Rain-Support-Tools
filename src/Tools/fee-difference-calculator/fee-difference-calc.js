de5231b456b1e2dee399aaa02fb90bbb049eab23
Robert <robert.hendrickson15@gmail.com>
2025-05-12 10:10:24 -0600

    moved all functions into app

--- src/Tools/fee-difference-calculator/fee-difference-calc.js
+++ src/Tools/fee-difference-calculator/fee-difference-calc.js
@@ -52,74 +52,64 @@ const feeDifferenceCalculator = createApp({
         closeError(){
             this.showWait = false;
             this.errorMessage = '';
+        },
+        precentToDecimal(value){
+            value = '0.0' + value.replace('.','');
+            value = parseFloat(value);
+            return value;
+        },
+        calculateDifference(){
+            let rates = {};
+            //collect and store card rates and fees
+            rates['amexp'] = [this.precentToDecimal(document.querySelector('#amexcardPresentRate').value),parseFloat(document.querySelector('#amexcardPresentAmount').value)];
+            rates['amexnp'] = [this.precentToDecimal(document.querySelector('#amexcardNotPresentRate').value),parseFloat(document.querySelector('#amexcardNotPresentAmount').value)];
+            rates['cardp'] = [this.precentToDecimal(document.querySelector('#cardPresentRate').value),parseFloat(document.querySelector('#cardPresentAmount').value)];
+            rates['cardnp'] = [this.precentToDecimal(document.querySelector('#cardNotPresentRate').value),parseFloat(document.querySelector('#cardNotPresentAmount').value)];
+            let table_data = document.querySelectorAll('#transactions tbody tr');
+            for(let i=0;i<table_data.length;i++){
+                let temp_row = table_data[i];
+                if(temp_row.querySelector('td:nth-child(3)').innerText === 'Card Not Present' && temp_row.querySelector('td:nth-child(6)').innerText !='$0.00'){
+                    let temp_rates = rates.cardp;
+                    let temp_collected = temp_row.querySelector('td:nth-child(6)').innerText.replace('$','');
+                    if(this.amexRates){
+                        //needs to check if rate used on row matches non-amex rates if it does do nothing. if it doesn't then we need to update temp_rates to use amex present rates instead.
+                        let check_rate = rates.cardnp;
+                        let check_fee = parseFloat(temp_collected);
+                        check_fee *= check_rate[0];
+                        check_fee += check_rate[1];
+                        if(check_fee.toFixed(2) != temp_row.querySelector('td:nth-child(7)').innerText.replace('-$','')){
+                            temp_rates = rates.amexp;
+                        };
+                    };
+                    let temp_fee = parseFloat(temp_collected) * temp_rates[0];
+                    temp_fee += temp_rates[1];
+                    let difference = parseFloat(temp_row.querySelector('td:nth-child(7)').innerText.replace('$',''));
+                    difference += temp_fee;
+                    temp_row.querySelector('td:nth-child(10)').innerText = '$' + temp_fee.toFixed(2);
+                    temp_row.querySelector('td:last-child').innerText = '$' + difference.toFixed(2);
+                }else{
+                    temp_row.querySelector('td:nth-child(10)').innerText = '$0.00';
+                    temp_row.querySelector('td:last-child').innerText = '$0.00';
+                };
+            };
+            this.calculateTotals(table_data);
+        },
+        calculateTotals(table){
+            let total_fees = 0;
+            let total_calculated_fees = 0;
+            let total_difference = 0;
+            for(let i=0;i<table.length;i++){
+                if(table[i].querySelector('td:nth-child(10)').innerText != '$0.00'){
+                    total_fees += parseFloat(table[i].querySelector('td:nth-child(7)').innerText.replace('$',''));
+                    total_calculated_fees += parseFloat(table[i].querySelector('td:nth-child(10)').innerText.replace('$',''));
+                    total_difference += parseFloat(table[i].querySelector('td:nth-child(11)').innerText.replace('$',''));
+                };
+            };
+            let total_table = document.querySelector('#totals tbody tr');
+            total_table.querySelector('td:nth-child(1)').innerText = `$${total_fees.toFixed(2)}`;
+            total_table.querySelector('td:nth-child(2)').innerText = `$${total_calculated_fees.toFixed(2)}`;
+            total_table.querySelector('td:nth-child(3)').innerText = `$${total_difference.toFixed(2)}`;
         }
     }
 });
-window.feeDifferenceCalculator = feeDifferenceCalculator.mount('#fee-difference-calculator');
-/*takes a string from rates boxes and converts it to a usable decimal for calculation*/
-function precentToDecimal(value){
-    value = '0.0' + value.replace('.','');
-    value = parseFloat(value);
-    return value;
-};
-
-/*loops through table and adds info to last two cells of each row*/
-function calculateDifference(){
-    let rates = {};
-    //collect and store card rates and fees
-    rates['amexp'] = [precentToDecimal(document.querySelector('#amexcardPresentRate').value),parseFloat(document.querySelector('#amexcardPresentAmount').value)];
-    rates['amexnp'] = [precentToDecimal(document.querySelector('#amexcardNotPresentRate').value),parseFloat(document.querySelector('#amexcardNotPresentAmount').value)];
-    rates['cardp'] = [precentToDecimal(document.querySelector('#cardPresentRate').value),parseFloat(document.querySelector('#cardPresentAmount').value)];
-    rates['cardnp'] = [precentToDecimal(document.querySelector('#cardNotPresentRate').value),parseFloat(document.querySelector('#cardNotPresentAmount').value)];
-    let table_data = document.querySelectorAll('#transactions tbody tr');
-    for(let i=0;i<table_data.length;i++){
-        let temp_row = table_data[i];
-        if(temp_row.querySelector('td:nth-child(3)').innerText === 'Card Not Present' && temp_row.querySelector('td:nth-child(6)').innerText !='$0.00'){
-            let temp_rates = rates.cardp;
-            let temp_collected = temp_row.querySelector('td:nth-child(6)').innerText.replace('$','');
-            if(document.querySelector('#amexCheck').checked){
-                //needs to check if rate used on row matches non-amex rates if it does do nothing. if it doesn't then we need to update temp_rates to use amex present rates instead.
-                let check_rate = rates.cardnp;
-                let check_fee = parseFloat(temp_collected);
-                check_fee *= check_rate[0];
-                check_fee += check_rate[1];
-                if(check_fee.toFixed(2) != temp_row.querySelector('td:nth-child(7)').innerText.replace('-$','')){
-                    temp_rates = rates.amexp;
-                };
-            };
-            let temp_fee = parseFloat(temp_collected) * temp_rates[0];
-            temp_fee += temp_rates[1];
-            let difference = parseFloat(temp_row.querySelector('td:nth-child(7)').innerText.replace('$',''));
-            difference += temp_fee;
-            temp_row.querySelector('td:nth-child(10)').innerText = '$' + temp_fee.toFixed(2);
-            temp_row.querySelector('td:last-child').innerText = '$' + difference.toFixed(2);
-        }else{
-            temp_row.querySelector('td:nth-child(10)').innerText = '$0.00';
-            temp_row.querySelector('td:last-child').innerText = '$0.00';
-        };
-    };
-    calculateTotals(table_data);
-};
-
-/*loops through table to compile totals*/
-function calculateTotals(table){
-    let total_fees = 0;
-    let total_calculated_fees = 0;
-    let total_difference = 0;
-    for(let i=0;i<table.length;i++){
-        if(table[i].querySelector('td:nth-child(10)').innerText != '$0.00'){
-            total_fees += parseFloat(table[i].querySelector('td:nth-child(7)').innerText.replace('$',''));
-            total_calculated_fees += parseFloat(table[i].querySelector('td:nth-child(10)').innerText.replace('$',''));
-            total_difference += parseFloat(table[i].querySelector('td:nth-child(11)').innerText.replace('$',''));
-        };
-    };
-    let total_table = document.querySelector('#totals tbody tr');
-    total_table.querySelector('td:nth-child(1)').innerText = `$${total_fees.toFixed(2)}`;
-    total_table.querySelector('td:nth-child(2)').innerText = `$${total_calculated_fees.toFixed(2)}`;
-    total_table.querySelector('td:nth-child(3)').innerText = `$${total_difference.toFixed(2)}`;
-};
-//set event listeners for buttons
-document.addEventListener('DOMContentLoaded', () => {
-    //Run calculation
-    document.querySelector('#amexSelector input#calculate').addEventListener('click', calculateDifference);
-});
+feeDifferenceCalculator.mount('#fee-difference-calculator');
