95fcbdd8f0e1f4a7ce520c932d89f812230a7bea
Robert <robert.hendrickson15@gmail.com>
2025-06-12 17:11:48 -0600

    made some updates to record editor

--- src/Tools/dns-help/record-editor.js
+++ src/Tools/dns-help/record-editor.js
@@ -203,11 +203,76 @@ export default {
                     error_list['serverHost'] = 'SRV record Server Host needs to be a domain. (www.domain.com)';
                 }
             }
+            if(this.isCorrection){
+                //check new values are valid
+                //check type is selected
+                if(this.record.newType === ''){
+                    error_list['type'] = 'Type is required';
+                }
+                //check name is entered
+                if(this.record.newName === ''){
+                    error_list['name'] = 'Name is required. If the data given to you by a customer has no value for the Name, please confirm with the customer that this is intentional. If confirmed please enter "@" as the value.';
+                }
+                //check value data based on type
+                if(this.record.newType === 'A'){
+                    if(!(/^(?:\d{1,3}\.){3}\d{1,3}$/).test(this.record.newValue)){
+                        error_list['value'] = 'A record value needs to be an ipv4 address. (1.1.1.1)';
+                    }
+                }
+                if(this.record.newType === 'AAAA'){
+                    if(!(/^(?:[a-zA-z0-9]{4}\:){7}[a-zA-z0-9]{4}$/).test(this.record.newValue)){
+                        error_list['value'] = 'AAAA record value needs to be an ipv6 address. (2001:0000:130F:0000:0000:09C0:876A:130B)';
+                    }
+                }
+                if(this.record.newType === 'CNAME'){
+                    if(!(/^(?:[\w\-]+\.)+[\w\-]+\.\w{2,}\.?$/).test(this.record.newValue)){
+                        error_list['value'] = 'CNAME record value needs to be a domain. (www.domain.com)';
+                    }
+                }
+                if(this.record.newType === 'TXT'){
+                    if(this.record.newValue === ''){
+                        error_list['value'] = 'TXT record value is required';
+                    }
+                }
+                if(this.record.newType === 'PTR'){
+                    if(this.record.newName === '' || !(/(?:\d{1,3}\.){3}\d{1,3}$/).test(this.record.newName)){
+                        error_list['name'] = 'PTR Name value needs to be an ipv4 address. (1.0.0.1)';
+                    }
+                    if(!(/^(?:[\w\-]+\.)+[\w\-]+\.\w{2,}\.?$/).test(this.record.newValue)){
+                        error_list['value'] = 'PTR needs to be a domain (www.domain.com)';
+                    }
+                }
+                if(this.record.newType === 'MX'){
+                    if(!(/^\d+$/).test(this.record.newPriority)){
+                        error_list['priority'] = 'MX record priority needs to be a number. (10)';
+                    }
+                    if(!(/^(?:[\w\-]+\.)+[\w\-]+\.\w{2,}\.?$/).test(this.record.newMailHostName)){
+                        error_list['mailHostName'] = 'MX record Mail Host needs to be a domain. (mail.domain.com)';
+                    }
+                }
+                if(this.record.newType === 'SRV'){
+                    if(!(/^\d+$/).test(this.record.newPriority)){
+                        error_list['priority'] = 'SRV record priority needs to be a number. (10)';
+                    }
+                    if(!(/^\d+$/).test(this.record.newWeight)){
+                        error_list['weight'] = 'SRV record weight needs to be a number. (10)';
+                    }
+                    if(!(/^\d+$/).test(this.record.newPort)){
+                        error_list['port'] = 'SRV record port needs to be a number. (10)';
+                    }
+                    if(!(/^(?:[\w\-]+\.)+[\w\-]+\.\w{2,}\.?$/).test(this.record.newServerHost)){
+                        error_list['serverHost'] = 'SRV record Server Host needs to be a domain. (www.domain.com)';
+                    }
+                }
+            }
             if(Object.keys(error_list).length > 0){
                 this.$refs.errorCtrl.updateErrorObject(error_list);
             }else{
                 if(this.record.ttl === ''){
                     this.record.ttl = 3600;
+                }
+                if(this.record?.newTtl === ''){
+                    this.record.newTtl = 3600;
                 }
                 this.submitRecord();
             }
