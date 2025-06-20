5393beb73683237e9a4675ffa32821fd982528a1
Robert <robert.hendrickson15@gmail.com>
2025-06-20 16:15:47 -0600

    updated navigation to use better handling

--- src/common/navigation/toolNavTemplate.js
+++ src/common/navigation/toolNavTemplate.js
@@ -1,8 +1,27 @@
-export const toolNavTemplate = `<div id="nav-toggle" @click="toggleNav">
+export const toolNavTemplate = `<div id="nav-toggle" @click="this.showNav = !this.showNav">
     <span class="fa-solid fa-bars"></span>
 </div>
-<div class="secondary_menu_links">
+<div class="secondary_menu_links" :class="this.showNav ? 'active' : ''">
     <div class="toolButtonsList">
+        <div>
+            <div class="nav-header">General</div>
+            <ul>
+                <li class="toolButton">
+                    <a href="/Rain-Support-Tools/">
+                        <div>
+                            <span class="fa-solid fa-house"></span>Home
+                        </div>
+                    </a>
+                </li>
+                <li class="toolButton">
+                    <a href="https://github.com/Robert-Hendrickson/Rain-Support-Tools/pulls?q=is%3Amerged" target="blank">
+                        <div>
+                            <span class="fa-solid fa-wrench"></span>System Updates
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
         <div v-for="nav_item in nav_list">
             <div class="nav-header">{{nav_item.title}}</div>
             <ul>
