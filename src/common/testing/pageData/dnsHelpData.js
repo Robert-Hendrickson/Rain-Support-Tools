70dac440dc3c0c5b4797aaa3e582f08d82d3262b
Robert <robert.hendrickson15@gmail.com>
2025-06-23 15:49:50 -0600

    updated testing files to work with new formatting

--- src/common/testing/pageData/dnsHelpData.js
+++ src/common/testing/pageData/dnsHelpData.js
@@ -1,99 +1,11 @@
 export default async function setDNSHelpData() {
-    document.getElementById('domain').value = 'rainadmin.com';
-    document.querySelectorAll('[choice-selector] div').forEach(function (el){
-        el.click();
-    });
-    document.querySelector(`#add-record-table tbody`).insertAdjacentHTML('beforeend', `<tr>
-        <td>
-            <span class="row-delete">X</span>
-        </td>
-        <td>
-            Type:<br>
-            <div class="data-input">TXT</div>
-        </td>
-        <td>
-            Name:<br>
-            <div class="data-input">@</div>
-        </td>
-        <td>
-            Value:<br>
-            <div class="data-input">_newdata</div>
-        </td>
-        <td style="width: 100px;">
-            TTL:<br>
-            <div class="data-input">3600</div>
-        </td>
-    </tr>`);
-    document.querySelector('#correct-record-table tbody').insertAdjacentHTML('beforeend', `<tr>
-        <td>
-            <span class="row-delete">X</span>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>
-                            Original Values
-                        </td>
-                        <td>
-                            Type:<br>
-                            <div class="data-input">MX</div>
-                        </td>
-                        <td>
-                            Name:<br>
-                            <div class="data-input">www</div>
-                        </td>
-                        <td>
-                            Value:<br>
-                            <div class="data-input">1 mail.host.com</div>
-                        </td>
-                        <td>
-                            TTL:<br>
-                            <div class="data-input">1200</div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            New Values
-                        </td>
-                        <td>
-                            Type:<br>
-                            <div class="data-input">MX</div>
-                        </td>
-                        <td>
-                            Name:<br>
-                            <div class="data-input">www</div>
-                        </td>
-                        <td>
-                            Value:<br>
-                            <div class="data-input">2 mail.host1.com</div>
-                        </td>
-                        <td>
-                            TTL:<br>
-                            <div class="data-input">3600</div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </td>
-    </tr>`);
-    document.querySelector('#remove-record-table tbody').insertAdjacentHTML('beforeend', `<tr>
-        <td>
-            <span class="row-delete">X</span>
-        </td>
-        <td>
-            Type:<br>
-            <div class="data-input">SRV</div>
-        </td>
-        <td>
-            Name:<br>
-            <div class="data-input">www</div>
-        </td>
-        <td>
-            Value:<br>
-            <div class="data-input">1 24 445 host.server.net</div>
-        </td>
-        <td style="width: 100px;">
-            TTL:<br>
-            <div class="data-input">3600</div>
-        </td>
-    </tr>`);
+    // Helper function to simulate user input
+    const simulateUserInput = (elementId, value) => {
+        const element = document.getElementById(elementId);
+        element.value = value;
+        element.dispatchEvent(new Event('input', { bubbles: true }));
+        element.dispatchEvent(new Event('change', { bubbles: true }));
+    };
+    simulateUserInput('domain', 'rainadmin.com');
+    document.querySelectorAll('[choice-selector] div').forEach(el => el.click());
 }
--- src/common/testing/pageData/siteWorkData.js
+++ src/common/testing/pageData/siteWorkData.js
@@ -1,3 +1,10 @@
 export default async function setSiteWorkData() {
-    document.getElementById('crm').value = '6085';
+    // Helper function to simulate user input
+    const simulateUserInput = (elementId, value) => {
+        const element = document.getElementById(elementId);
+        element.value = value;
+        element.dispatchEvent(new Event('input', { bubbles: true }));
+        element.dispatchEvent(new Event('change', { bubbles: true }));
+    };
+    simulateUserInput('crm', '6085');
 }
