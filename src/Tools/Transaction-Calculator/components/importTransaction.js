8f63b393d4026c8261d759443cd95de37162b073
RobertHend <robert.hendrickson15@gmail.com>
2025-11-03 08:35:34 -0700

    adjusted inport functionality to remove negative values from discount amounts so that the calculator can calculate correctly.

--- src/Tools/Transaction-Calculator/components/importTransaction.js
+++ src/Tools/Transaction-Calculator/components/importTransaction.js
@@ -27,7 +27,7 @@ export default {
                     line_items.push({
                         quantity: transaction_data[i].quantity,
                         price: transaction_data[i].price,
-                        discount: parseFloat(transaction_data[i].total_discount_amount),
+                        discount: this.getDiscountAmount(transaction_data[i]),
                         type: type,
                     });
                 }
@@ -35,6 +35,17 @@ export default {
             this.$emit('import-transaction-data', line_items);
             this.$emit('close-import-transaction');
             this.import_data = '';
+        },
+        getDiscountAmount(line_item) {
+            if (typeof(line_item.total_discount_amount) != 'number') {
+                return 0;
+            }
+
+            if (line_item.total_discount_amount < 0) {
+                return line_item.total_discount_amount * -1;
+            }
+
+            return line_item.total_discount_amount;
         },
         doesLineItemMeetCriteria(line_item) {
             return TRANSACTION_TYPES.materialTypes.includes(line_item.type.toLowerCase()) ? 'material' : TRANSACTION_TYPES.serviceTypes.includes(line_item.type.toLowerCase()) ? 'service' : TRANSACTION_TYPES.classTypes.includes(line_item.type.toLowerCase()) ? 'class' : TRANSACTION_TYPES.shippingTypes.includes(line_item.type.toLowerCase()) ? 'shipping' : '';
