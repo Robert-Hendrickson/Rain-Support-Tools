6800b5247d6e294e6c6795358f8102526e08eca9
Robert Hendrickson <robert.hendrickson15@gmail.com>
2026-03-10 13:59:40 -0600

    added a check on file select to stop it from adding files that can't upload without chunking.

--- src/Tools/share-point/auth-config.js
+++ src/Tools/share-point/auth-config.js
@@ -3,5 +3,15 @@ export const config = {
     clientId: atob('ZTEwNGRmMDgtMDFkMi00Njc0LWExZGEtMDgwZjQ0Njc4NDc0'),
     redirectUri: 'https://robert-hendrickson.github.io/Rain-Support-Tools/src/Tools/share-point/callback.html',
     testRedirectUri: 'http://localhost/Rain-Support-Tools/src/Tools/share-point/callback.html',
-    scopes: 'user.read files.readwrite'
+    testPort: 'http://localhost:3000/Rain-Support-Tools/src/Tools/share-point/callback.html',
+    scopes: 'user.read files.readwrite',
+    getRedirectUri() {
+        if (window.location.hostname != 'localhost') {
+            return this.redirectUri;
+        } else if (window.location.hostname === 'localhost' && window.location.port === '3000') {
+            return this.testPort;
+        } else {
+            return this.testRedirectUri;
+        }
+    }
 };
